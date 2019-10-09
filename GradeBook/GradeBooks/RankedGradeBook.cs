b010e9e899289cd4e5b5cffb548dc67abede840e
Matthew Sims <msims@intertech.com>
2019-10-09 10:44:52 -0500

    Override statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -42,5 +42,26 @@ namespace GradeBook.GradeBooks
                 return 'F';
             }
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least five students");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least five students.");
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
